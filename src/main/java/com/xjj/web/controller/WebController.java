0bfd766c116bb729983e5e9a99ae3d23e2eee6d2
Cheng <chengxiangdong@outlook.com>
2017-08-12 13:49:48 +0800

    init

---
+++ src/main/java/com/xjj/web/controller/WebController.java
@@ -0,0 +1,26 @@
+package com.xjj.web.controller;
+
+import com.xjj.config.MyProps;
+import com.xjj.util.XDateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+	@Autowired
+	private MyProps myProps;
+	
+	@RequestMapping("/hi")
+	public String hi(Map<String, Object> model) {
+		model.put("time", XDateUtils.nowToString());
+		model.put("message", "这是测试的内容。。。");
+		model.put("toUserName", "张三");
+		model.put("fromUserName", "老许");
+		model.put("ArrayProps", myProps.getArrayProps());
+		return "welcome"; //自动寻找resources/templates中名字为welcome.ftl/welcome.vm的文件作为模板，拼装后返回
+	}
+}
